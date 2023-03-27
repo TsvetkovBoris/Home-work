615d5bf43c1d0db53d15be9d3d258b76c12780ec
Boris <Tsvetkovboris96@yandex.ru>
2023-03-27 20:24:21 +0300

    задача 2

---
+++ Ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.Write("Введите первое число = ");
+int num1 = Convert.ToInt32(Console.ReadLine());;
+System.Console.Write("Введите второе число = ");
+int num2 = Convert.ToInt32(Console.ReadLine());;
+System.Console.Write("Введите третье число = ");
+int num3 = Convert.ToInt32(Console.ReadLine());;
+if (num1 > num2 && num1 > num3)
+  {
+  Console.WriteLine("Максимальное число = " + num1);
+  }
+else 
+  if (num2 > num1 && num2 > num3)
+  {
+  Console.WriteLine("Максимальное число число = " + num2);
+  }
+  else 
+    if (num3 > num1 && num3 > num2)
+    {
+    Console.WriteLine("Максимальное число число = " + num3);
+    }
