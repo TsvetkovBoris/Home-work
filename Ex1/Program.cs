924941c99446e8b91404b7aae77ec2d3ce7e3d07
Boris <Tsvetkovboris96@yandex.ru>
2023-03-27 21:21:55 +0300

    Change

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,6 +1,8 @@
-﻿System.Console.Write("Введите первое число = ");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число = ");
 int num1 = Convert.ToInt32(Console.ReadLine());;
-System.Console.Write("Введите первое число = ");
+Console.Write("Введите первое число = ");
 int num2 = Convert.ToInt32(Console.ReadLine());;
 if (num1 > num2)
   {
--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -1,8 +1,10 @@
-﻿System.Console.Write("Введите первое число = ");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+Console.Write("Введите первое число = ");
 int num1 = Convert.ToInt32(Console.ReadLine());;
-System.Console.Write("Введите второе число = ");
+Console.Write("Введите второе число = ");
 int num2 = Convert.ToInt32(Console.ReadLine());;
-System.Console.Write("Введите третье число = ");
+Console.Write("Введите третье число = ");
 int num3 = Convert.ToInt32(Console.ReadLine());;
 if (num1 > num2 && num1 > num3)
   {
