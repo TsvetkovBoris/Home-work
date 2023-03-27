a4e6bbbaa0ec4e51e075761ef64dc61ca2f9f402
Boris <Tsvetkovboris96@yandex.ru>
2023-03-27 19:56:46 +0300

    Найдены максимальное и минимальное числа

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("   ");
+﻿System.Console.Write("Введите первое число = ");
+int num1 = Convert.ToInt32(Console.ReadLine());;
+System.Console.Write("Введите первое число = ");
+int num2 = Convert.ToInt32(Console.ReadLine());;
+if (num1 > num2)
+  {
+  Console.WriteLine("Максимальное число = " + num1);
+  Console.WriteLine("Минимальное число = " + num2);
+  }
+else 
+  {
+  Console.WriteLine("Максимальное число число = " + num2);
+  Console.WriteLine("Минимальное число = " + num1);
+  }
