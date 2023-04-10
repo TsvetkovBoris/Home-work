84d06edb4888d32e195efb9dbf3c43fd14ac4397
Boris <Tsvetkovboris96@yandex.ru>
2023-04-10 19:39:26 +0300

    Задача 8

--- Ex4/Program.cs
+++ Ex4/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
 
 Console.Write("Введите любое число: ");
-int N = int.Parse(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
 int i = 1;
 while (i <= N)
 {
